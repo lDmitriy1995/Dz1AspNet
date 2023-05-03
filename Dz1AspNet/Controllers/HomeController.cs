c3099c34432d881cab081b8bdf8b2824caaf0719
docenko.d <docenko.d@DIT-DDU>
2023-05-03 17:09:37 +0600

    Добавьте файлы проекта.

---
+++ Dz1AspNet/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Dz1AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Dz1AspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var osName = Environment.OSVersion.VersionString;
+            return Content(osName);
+
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
